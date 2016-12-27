1c675f25e5ef447336a15e85c71c56881595c8f7
jiang hong <jianghong@jianghongdeMacBook-Pro.local>
2016-12-27 18:46:48 +0800

    Ones and Zeroes

---
+++ python/474. Ones and Zeroes/from_internet.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findMaxForm(self, strs, m, n):
+        """
+        :type strs: List[str]
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[0] * (n + 1) for x in range(m + 1)]
+        for s in strs:
+            zero, one = s.count('0'), s.count('1')
+            for x in range(m, zero - 1, -1):
+                for y in range(n, one - 1, -1):
+                    print s,x,y
+                    dp[x][y] = max(dp[x - zero][y - one] + 1, dp[x][y])
+                    print dp
+        return dp[m][n]
+
+
+solution = Solution()
+print solution.findMaxForm(["10","0001","111001","1","0"],5,3)
